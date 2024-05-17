e5ec810ae233d84e352ad3972a6d771125e91c63
mahedi1318 <mahedi.mern@gmail.com>
2024-05-17 21:39:12 +0600

    init try kora hoise

--- src/components/Hero__part2/HeroTwo.jsx
+++ src/components/Hero__part2/HeroTwo.jsx
@@ -7,7 +7,9 @@ const HeroTwo = () => {
         <div className='py-8'>
             <div className="container">
                 <div className="grid grid-cols-4 gap-8">
-                    <div className='py-10 pl-5 h-[320px] bg-gradient-to-br from-black/90 to-black/70 text-white rounded-3xl relative flex items-end' ></div>
+                    <div className='py-10 pl-5 h-[320px] bg-gradient-to-br from-black/90 to-black/70 text-white rounded-3xl relative flex items-end' >
+                        <h2 className='text-white'>mahedi</h2>
+                    </div>
                     <div className='py-10 pl-5 h-[320px] bg-gradient-to-br from-yellow-700/90 to-black/70 text-white rounded-3xl relative flex items-end' ></div>
                     <div className='py-10 col-span-2 pl-5 h-[320px] bg-gradient-to-br from-gray-700/90  to-black/70 text-white rounded-3xl relative flex items-end' ></div>
                 </div>
